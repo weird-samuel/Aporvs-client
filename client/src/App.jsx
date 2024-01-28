1710fb79f3cf96091a87a08952afe718a3fc0c6d
weird_samuel <fiyinfoluwasamuel580@gmail.com>
2024-01-28 15:47:19 +0100

    meh

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
 import "./App.css";
 const App = () => {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="bg-green-300 p-16">
+      <h1 className="text-red">Hello</h1>
     </div>
   );
 };
